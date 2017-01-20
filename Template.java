4f41ff17d7ebe59266bfca6be7efff353c6934fc
Cody Swift <ccswift@me.com>
2017-01-20 17:15:36 -0500

    added basic template with header

---
+++ Template.java
@@ -0,0 +1,20 @@
+/*
+
+Name: Cameron Swift
+
+Date: 01/24/2017
+
+Project: Project 01
+
+File: Template.java
+
+*/
+
+
+public class Template {
+  public static void main(String[] args){
+    
+  }
+  
+  
+}
